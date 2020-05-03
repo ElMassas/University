e3291ea9fbb9bace74489737a45a5ed16ff50005
ElMassas <mfgluis1997@gmail.com>
2020-05-03 21:03:17 +0100

    solving merge conflict with add class

--- LP/LP_A1_37555_38176/src/Operations/Arithmetic/Add.java
+++ LP/LP_A1_37555_38176/src/Operations/Arithmetic/Add.java
@@ -6,12 +6,14 @@ import TISC.Machine;
 public class Add extends Operations {
 
     @Override
-    public void execute(Machine machine){
+    public void execute(Machine machine, String operator){
         //changes the values in the evaluation stack
         int t1 = machine.getEvaluationStack().pop();
         int t2 = machine.getEvaluationStack().pop();
-        machine.getEvaluationStack().push(t1 + t2);
+        machine.getEvaluationStack().push(t1 operator t2);
         machine.changePC(4); //4 bytes
     }
+
+    public void ex
 
 }
