c0f26cb599955d14fa65c654ff5365247f2fd4d1
ElMassas <mfgluis1997@gmail.com>
2020-05-03 21:37:53 +0100

    added auto-generated tests, updated makefile and built lexer and parser classes

--- LP/LP_A1_37555_38176/src/Operations/Arithmetic/Add.java
+++ LP/LP_A1_37555_38176/src/Operations/Arithmetic/Add.java
@@ -8,20 +8,9 @@ public class Add extends ArithmeticOperations{
     private final int PC_JP = 4; //4 bytes
 
     @Override
-    public void execute(Machine machine, String operator){
-        //changes the values in the evaluation stack
-<<<<<<< HEAD
-        int t1 = machine.getEvaluationStack().pop();
-        int t2 = machine.getEvaluationStack().pop();
-        machine.getEvaluationStack().push(t1 operator t2);
-        machine.changePC(4); //4 bytes
-=======
+    public void execute(Machine machine) {
         super.loadValues(machine);
         super.setValues(machine,this.t1 + this.t2 );
         machine.changePC(PC_JP);
->>>>>>> 844e986e34ce106b9cece845898615bf56afe67b
     }
-
-    public void ex
-
 }
